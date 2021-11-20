4e555fda5268851616494b7aaf0b6c650824a8be
Elias Junior <elijah.junnior@outlook.com>
2021-11-20 17:59:40 -0300

    home page changed to fix error that allowed subiscribed users to subscribe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -2,6 +2,7 @@ import styles from './styles.module.scss';
 import { signIn, useSession } from 'next-auth/client'
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
+import { useRouter } from 'next/router';
 
 interface SubscribeButtonProps {
     priceId: string
@@ -11,8 +12,13 @@ export function SubscribeButton(props: SubscribeButtonProps) {
 
     // pega os dados de login do Next auth
     const [session] = useSession()
+    // hookie usado para redirecionar o usuario para outra pagina
+    // usado quando o redirecionamento está sendo feito de forma programatica
+    // dentro de uma função, ou sejá, não causado por um click do usuario
+    const router = useRouter();
 
     async function handleSubscribe() {
+
         // verifica se o usuario esta logado
         if (!session) {
             //caso não esteja logado 
@@ -20,6 +26,14 @@ export function SubscribeButton(props: SubscribeButtonProps) {
             signIn('github');
             return;
         }
+
+        // verifica e o usuario já está inscrito
+        if (session.activeSubscription) {
+            // caso já esteja inscrito o redireciona para o pagina de posts
+            router.push('/posts');
+            return;
+        }
+
         try {
             // efetua consumo que cria o checkou do stripe 
             const response = await api.post('/subscribe');
