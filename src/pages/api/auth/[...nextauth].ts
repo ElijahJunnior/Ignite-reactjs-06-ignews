645e52a09d9dc4d03b9e901d37c4868b3c4525d5
Elias Junior <elijah.junnior@outlook.com>
2021-11-20 17:57:34 -0300

    authentication api changed to get and propagate subscription data

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -12,9 +12,53 @@ export default NextAuth({
         }),
     ],
     callbacks: {
+        // esse callback está sendo usado para adicionar mais informações a contexto session
+        // assim outras informações de usuario podem ser propagados junto com a session
+        async session(session) {
+            try {
+                // buscar na base de dados uma inscrição que pertença ao usuario logado e que esteja ativa
+                const userActiveSubscription = await fauna.query(
+                    q.Get(
+                        q.Intersection([
+                            q.Match(
+                                q.Index('subscription_by_user_ref'),
+                                // filtra u
+                                q.Select(
+                                    'ref',
+                                    q.Get(
+                                        q.Match(
+                                            q.Index('user_by_email'),
+                                            q.Casefold(session.user.email)
+                                        )
+                                    )
+                                )
+                            ),
+                            q.Match(
+                                q.Index('subscription_by_status'),
+                                'active'
+                            )
+
+                        ])
+                    )
+                )
+                // retorna um novo objeto contendo todas as propriedade da Session e os dados do inscrição
+                return {
+                    ...session,
+                    activeSubscription: userActiveSubscription
+                };
+            } catch (err) {
+                // caso o fauna não escontro nenhum registro ele irá disparar um erro
+                // necessa caso a função irá retornar as dados da Session e null no lugar a inscrição
+                return {
+                    ...session,
+                    activeSubscription: null
+                }
+            }
+        },
         async signIn(user, account, profile) {
             const { email } = user;
             try {
+                // verifica se o usuario existe na base e o cria caso não exista
                 await fauna.query(
                     q.If(
                         q.Not(
