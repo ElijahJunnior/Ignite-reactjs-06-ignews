631d3c90c0f4f220dee1dfd84ff53b78b7c63a80
Elias Junior <elijah.junnior@outlook.com>
2022-05-23 09:27:20 -0300

    fix issues in jest settings

--- src/components/ActiveLink/ActiveLink.spec.tsx
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -1,13 +1,31 @@
 import { render } from '@testing-library/react';
 import { ActiveLink } from '.';
 
+// cria um mock para imitar um modulo - no caso do exemplo next/router
+jest.mock('next/router', () => { 
+  return { 
+    // função fake do modulo imitada
+    useRouter() { 
+      return { 
+        // retornando um valor para a função que seja conveniente para o teste
+        asPath: '/'
+      }
+    }
+  }
+})
+
 test('active link renders correctly', () => { 
   // a função render renderiza o componente de forma virtual, 
   // para que o teste possa ver seu conteudo 
-  const { debug } = render( 
-    <ActiveLink href="/"  activeClassName='active'>
+  const { 
+    // retorna o html gerado pela função render
+    debug 
+  } = render( 
+    <ActiveLink href="/" activeClassName='active'>
       <a>Home</a>
     </ActiveLink>
   )
-  debug();
+  // debug();
+
+
 })
