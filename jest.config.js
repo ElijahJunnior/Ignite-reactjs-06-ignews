36f5a3a39b722ea907b1f125cde7a9a16c1e43ad
Elias Jr <elijah.junnior@outlook.com>
2022-05-23 07:39:29 -0300

    preparing application to use jest

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = { 
+  // pastas que devem ser ignoradas porque não vão conter testes
+  testIgnorePatterns: ["/node_modules/", "/.next/"], 
+  // arquivos que devem ser executados pelo jest
+  setupFilesAfterEnv: [
+    "<roodDir>/src/tests/setupTests.ts"
+  ],
+  // define como serão os arquivos em typescript convertidos
+  // isso é necessário porque o jest não reconhece o typescript
+  transform: { 
+    // espressão regular - todos os arquivos com 1 ou mais caracteres no nome
+    // que tenham ponto no nome e termina com (js, jsx, ts, tsx)
+    // Apos os 2 pontos é informado o pacote que vai ser usado para a transformação
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
+  }, 
+  // informa que ambiente o jest está trabalhando
+  // ele vai usar isso para saber como simular o ambiente durante os testes
+  // o jsdom é uma forma nativa de fazer isso
+  // ele entrega para o jest o que está sendo exibido em tela porem em javascript
+  testEnvironment: 'jsdom'
+};
